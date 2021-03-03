f483290fe73895b800fb1ba9e281334ecfecaa65
mcpoley <mcpoley@gmail.com>
2021-03-03 16:18:18 -0500

    adding files

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,30 +18,24 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: satelliteStreets,
+  "Streets": streets,
+  "Satellite": satelliteStreets,
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
-})
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/mcpoley/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  //Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
 
    
